72e99911a4ad580b598d6cb6dc2d9177f1d037b7
Faramir <darth_faramir@proton.me>
2024-03-16 01:52:38 +0300

    feat: delete unsued config from lspconfig; add Hyprlang filetype to lspconfig

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -128,20 +128,6 @@ local config = function()
 		},
 	})
 
-	local luacheck = require("efmls-configs.linters.luacheck")
-	local stylua = require("efmls-configs.formatters.stylua")
-	local flake8 = require("efmls-configs.linters.flake8")
-	local black = require("efmls-configs.formatters.black")
-	local eslint = require("efmls-configs.linters.eslint")
-	local prettier_d = require("efmls-configs.formatters.prettier_d")
-	local fixjson = require("efmls-configs.formatters.fixjson")
-	local shellcheck = require("efmls-configs.linters.shellcheck")
-	local shfmt = require("efmls-configs.formatters.shfmt")
-	local hadolint = require("efmls-configs.linters.hadolint")
-	local solhint = require("efmls-configs.linters.solhint")
-	local cpplint = require("efmls-configs.linters.cpplint")
-	local clangformat = require("efmls-configs.formatters.clang_format")
-
 	-- configure efm server
 	lspconfig.efm.setup({
 		filetypes = {
@@ -172,29 +158,14 @@ local config = function()
 			codeAction = true,
 			completion = true,
 		},
-		settings = {
-			languages = {
-				lua = { luacheck, stylua },
-				python = { flake8, black },
-				typescript = { eslint, prettier_d },
-				json = { eslint, fixjson },
-				jsonc = { eslint, fixjson },
-				sh = { shellcheck, shfmt },
-				javascript = { eslint, prettier_d },
-				javascriptreact = { eslint, prettier_d },
-				typescriptreact = { eslint, prettier_d },
-				svelte = { eslint, prettier_d },
-				vue = { eslint, prettier_d },
-				markdown = { prettier_d },
-				docker = { hadolint, prettier_d },
-				solidity = { solhint },
-				html = { prettier_d },
-				css = { prettier_d },
-				c = { clangformat, cpplint },
-				cpp = { clangformat, cpplint },
-			},
-		},
 	})
+
+  -- Hyprlang
+  lspconfig.hyprlang.setup ({
+    capabilities = capabilities,
+		on_attach = on_attach,
+		filetypes = { ".*/hypr/.*%.conf" },
+  })
 end
 
 return {
