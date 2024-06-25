6c5f9de007b0f02e262221e78b4f39f785595722
Sam Pearse <sp_xand3r@proton.me>
2024-06-25 21:54:26 +0300

    feat: change theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -4,7 +4,7 @@ return {
 	lazy = false,
 	priority = 999,
 	config = function()
-		vim.cmd("colorscheme catppuccin-latte") -- catppuccin-latte, catppuccin-frappe, catppuccin-macchiato, catppuccin-mocha
+		vim.cmd("colorscheme catppuccin-mocha") -- catppuccin-latte, catppuccin-frappe, catppuccin-macchiato, catppuccin-mocha
 		require("catppuccin").setup({})
 	end,
 }
